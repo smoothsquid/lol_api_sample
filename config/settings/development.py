3545aad79b475002034aa0bffcfd29b34159ce16
Jake Lee <jakeogtgg@gmail.com>
2021-02-17 23:46:35 +0900

    소환사 아이콘 추가

--- config/settings/development.py
+++ config/settings/development.py
@@ -3,3 +3,51 @@ from .settings import *
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "formatters": {
+        "django.server": {
+            "()": "django.utils.log.ServerFormatter",
+            "format": "[{server_time}] {message}",
+            "style": "{",
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "filters": ["require_debug_true"],
+            "class": "logging.StreamHandler",
+        },
+        "django.server": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "django.server",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "mail_admins"],
+            "level": "DEBUG",
+        },
+        "django.server": {
+            "handlers": ["django.server"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
--- lol/utils.py
+++ lol/utils.py
@@ -1,11 +1,17 @@
+import logging
+
 from . import api
+
+logger = logging.getLogger(__name__)
 
 
 def search_match(summoner_name):
     summoner = api.get_summoner_by_name(summoner_name)
     account_id = summoner["accountId"]
+
     result = api.get_matches_by_account(account_id)
     matches = result["matches"]
+
     if matches and len(matches) > 0:
         game_id = matches[0]["gameId"]
         match = api.get_match(game_id)
